ee08696cd425775e96149498af9d5b6b9a73d5f3
smithjosephm <36716873+smithjosephm@users.noreply.github.com>
2018-03-13 09:22:58 -0500

    Update slotmachine.py
    Update without Else statement

--- slotmachine.py
+++ slotmachine.py
@@ -7,16 +7,14 @@ def play():
     slot1=choice(slotsPossible)
     slot2=choice(slotsPossible)
     slot3=choice(slotsPossible)
-    lose = choice(Loser)
-    win = ""
+    sassyLoss = choice(Loser)
+    win = sassyLoss
     if (slot1==slot2==slot3=="cherry"):
         win = "\nYou win $100"
     if (slot1==slot2==slot3=="crown"):
         win = "\nYou win $50"
     if (slot1==slot2==slot3=="bar"):
         win = "\nYou win $5"
-    else:
-        win = lose
     return slot1+":"+slot2+":"+slot3+" "+win
 for i in range(int(numberOfTimes)):
     print(play())
